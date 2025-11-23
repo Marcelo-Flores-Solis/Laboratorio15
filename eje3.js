e65ad23775a43a7cdb617e5c1591f2674c117a53
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-11-22 20:59:35 -0500

    Paso 5: eje3 = Cambiar atributos src y alt

---
+++ eje3.js
@@ -0,0 +1,4 @@
+// Paso 5: Seleccionar la primera imagen y cambiar sus atributos src y alt
+const primeraImagen = document.querySelector('img');
+primeraImagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb4JybrOePfJAuLk7ij9lTaidKXhF43nMOHX5BJRYMwJxhdgvGxZ53_pgUUA-FbiG_zopSWxCBhCc3CklqLTusICjqn4kNJQfPqGBQxg&s";
+primeraImagen.alt = "Imagen modificada";
