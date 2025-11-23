3aa704d020b6abb9e5c259b998b12ef02b89f275
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-11-22 22:01:21 -0500

    Paso 18: eje16 = agregar atributo data-index

---
+++ eje16.js
@@ -0,0 +1,11 @@
+// Paso 18: Agrega data-index y lo muestra dentro de cada sección
+const secciones = document.querySelectorAll('section');
+secciones.forEach((section, idx) => {
+  section.setAttribute('data-index', idx + 1);
+
+// datos
+  const span = document.createElement('span');
+  span.textContent = ` (data-index: ${idx + 1})`;
+  span.style.color = "red";
+  section.appendChild(span);
+});
