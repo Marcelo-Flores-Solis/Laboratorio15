b3399b87b32ed57ecef118c2ce4ce02fa52ba457
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-11-22 21:18:45 -0500

    Paso 9: eje7 = Crear e insertar un parrafo

---
+++ eje7.js
@@ -0,0 +1,4 @@
+// Paso 9: Crear e insertar un nuevo <p> al final del <body>
+const nuevoParrafo = document.createElement('p');
+nuevoParrafo.textContent = "Elemento insertado con JS";
+document.body.appendChild(nuevoParrafo);
